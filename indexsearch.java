21900a229e933a90b463e01480209f28dcdeee4d
twinklechhillar <twinklechhillar89@gmail.com>
2026-07-09 08:44:29 +0530

    Create indexsearch.java

---
+++ indexsearch.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+     int[] arr = {10, 20, 30, 40, 50};
+     int target = 30;
+     boolean found = false;
+     for(int i=0; i<arr.length; i++){
+         if(arr[i]==target){
+           found = true;
+           System.out.println("found at index " + i );  
+           break;
+         }
+     }
+        
+    }
+}
